298d8cd6d564bda18708a160ad9df78c5440ab5b
kilo <alex.b.gust@gmail.com>
2022-09-28 14:40:57 -0700

    turn off for now

--- CfgFunctions.hpp
+++ CfgFunctions.hpp
@@ -101,4 +101,5 @@ class KPLIB {
     #include "scripts\client\CfgFunctions.hpp"
     #include "scripts\server\CfgFunctions.hpp"
     #include "BW\radio_chatter\functions.hpp"
+    //#include "BW\other_missions\functions.hpp" // uncomment when framework is done and tested
 };
